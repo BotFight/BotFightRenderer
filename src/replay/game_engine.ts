bf173200758192590d752af816c747841b48a279
Henry Liao <hliao62@gatech.edu>
2025-01-09 05:22:34 -0500

    Game replay WIP

---
+++ src/replay/game_engine.ts
@@ -0,0 +1,252 @@
+export class Map{
+    dim_x: number;
+    dim_y: number;
+    start_a: [number, number];
+    start_b: [number, number];
+    start_size: number;
+    min_player_size: number;
+    apple_timeline: number[][];
+    cells_walls: number[][];
+
+    constructor(map_string: string){
+        let infos:string[] = map_string.split("#");
+        let info_dim:string[] = infos[0].split(",");
+        this.dim_x = parseInt(info_dim[0]);
+        this.dim_y = parseInt(info_dim[1]);
+
+        let info_a:string[] = infos[1].split(",")
+        let info_b:string[] = infos[2].split(",")
+        this.start_a = [parseInt(info_a[0]), parseInt(info_a[1])]
+        this.start_b = [parseInt(info_b[0]), parseInt(info_b[1])]
+
+        this.start_size = parseInt(infos[3])
+        this.min_player_size = parseInt(infos[4])
+
+        let apples:string[] = infos[5].split("|")
+        this.apple_timeline = new Array(apples.length).fill(null).map(
+            () => new Array(3).fill(0));
+
+        for(let i = 0; i< apples.length; i++){
+            let apple:string[] = apples[i].split(',');
+            for(let j = 0; j < 3; j++){
+                this.apple_timeline[i][j] = parseInt(apple[j]);
+            }   
+        }
+
+        this.cells_walls = new Array(this.dim_y).fill(null).map(
+            () => new Array(this.dim_x).fill(0));
+
+        for(let i = 0; i< this.dim_y; i++){
+            for(let j = 0; j < this.dim_x; j++){
+                this.cells_walls[i][j] = parseInt(infos[6][i*this.dim_x+j]);
+            }   
+        }
+
+    }
+}
+
+
+
+
+class Snake{
+    apples_eaten: number;
+    head_loc: [number, number];
+    dir:Direction;
+
+    constructor(start_loc:[number, number], start_size:number){
+        this.apples_eaten = 0;
+        this.head_loc = start_loc;
+        this.dir = Direction.NORTH;
+    }
+
+    push_move(action:Direction): void {
+        this.dir = action;
+
+        switch (action){
+            case Direction.NORTH:
+                this.head_loc[1]-=1;
+                break;
+            case Direction.NORTHEAST:
+                this.head_loc[1]-=1;
+                this.head_loc[0]+=1;
+                break;
+            case Direction.EAST:
+                this.head_loc[0]+=1;
+                break;
+            case Direction.SOUTHEAST:
+                this.head_loc[1]+=1;
+                this.head_loc[0]+=1;
+                break;
+            case Direction.SOUTH:
+                this.head_loc[1]+=1;
+                break;
+            case Direction.SOUTHWEST:
+                this.head_loc[1]+=1;
+                this.head_loc[0]-=1;
+                break;
+            case Direction.WEST:
+                this.head_loc[0]-=1;
+                break;
+            case Direction.NORTHWEST:
+                this.head_loc[1]-=1;
+                this.head_loc[0]-=1;
+                break;
+
+        }
+
+    }
+        
+}
+
+export class Board {
+    map: Map;
+    cells_a: number[][];
+    cells_b: number[][];
+    cells_apples: number[][];
+    is_as_turn: boolean;
+    a_time: number;
+    b_time: number;
+    apple_counter: number;
+    turn_count:number;
+
+    snake_a:Snake;
+    snake_b:Snake;
+
+    constructor(map: Map, a_start:boolean, start_time:number){
+        this.map = map;
+        this.cells_a = new Array(map.dim_y).fill(null).map(
+            () => new Array(map.dim_x).fill(0));
+        this.cells_b = new Array(map.dim_y).fill(null).map(
+            () => new Array(map.dim_x).fill(0));
+        this.cells_apples = new Array(map.dim_y).fill(null).map(
+            () => new Array(map.dim_x).fill(0));
+
+        this.is_as_turn = a_start;
+        this.a_time = start_time;
+        this.b_time = start_time;
+        this.apple_counter = 0;
+        this.turn_count = 0;
+
+        this.snake_a = new Snake(map.start_a, map.start_size);
+        this.snake_b = new Snake(map.start_a, map.start_size);
+    }
+
+    play_turn(turn: Direction[], cells_lost:number[][], time:number): void{
+        if(this.is_as_turn){
+            turn.forEach((action, index)=>{
+                this.snake_a.push_move(action);
+
+                let x:number = this.snake_a.head_loc[0]
+                let y:number = this.snake_a.head_loc[1]
+                
+                if(this.cells_apples[y][x] > 0){
+                    this.snake_a.apples_eaten+=this.cells_apples[y][x];
+                    this.cells_apples[y][x] = 0;
+                }
+                
+            })
+
+            cells_lost.forEach((cell, index)=>{
+                this.cells_a[cell[1]][cell[0]]-=1;
+            })
+
+            this.a_time-=time;
+            
+        } else{
+            turn.forEach((action, index)=>{
+                this.snake_b.push_move(action);
+
+                let x:number = this.snake_b.head_loc[0]
+                let y:number = this.snake_b.head_loc[1]
+
+                if(this.cells_apples[y][x] > 0){
+                    this.snake_b.apples_eaten+=this.cells_apples[y][x];
+                    this.cells_apples[y][x] = 0;
+                }
+            })
+
+            cells_lost.forEach((cell, index)=>{
+                this.cells_b[cell[1]][cell[0]]-=1;
+            })
+
+
+            this.b_time-=time;
+
+        }       
+    }
+
+    next_turn(): void {
+        this.is_as_turn = !this.is_as_turn;
+        this.turn_count++;
+
+        while(this.apple_counter < this.map.apple_timeline.length 
+            && this.map.apple_timeline[this.apple_counter][0] <= 
+            this.get_round_num()){
+
+            let x:number = this.map.apple_timeline[this.apple_counter][1]
+            let y:number = this.map.apple_timeline[this.apple_counter][2]
+
+            this.cells_apples[y][x] = 1
+            this.apple_counter++;
+        }
+    }
+
+    get_round_num(): number {
+        return Math.trunc(this.turn_count/2);
+    }
+
+    get_enum_map(): number[][] {
+        let enum_map:number[][] = new Array(this.map.dim_y).fill(null).map(
+            () => new Array(this.map.dim_x).fill(0));
+
+        for(let i = 0; i< this.map.dim_y; i++){
+            for(let j = 0; j< this.map.dim_x; j++){
+                if(this.cells_apples[i][j]){
+                    enum_map[i][j] = 2;
+                } else if(this.map.cells_walls[i][j]){
+                    enum_map[i][j] = 1;
+                }
+                else if(this.cells_a[i][j]>0){
+                    if(this.snake_a.head_loc[0]==j && this.snake_a.head_loc[1]==i){
+                        enum_map[i][j] = 3;
+                    } else{
+                        enum_map[i][j] = 4;
+                    }
+                }else if(this.cells_b[i][j]>0){
+                    if(this.snake_b.head_loc[0]==j && this.snake_b.head_loc[1]==i){
+                        enum_map[i][j] = 5;
+                    } else{
+                        enum_map[i][j] = 6;
+                    }
+                } else{
+                    enum_map[i][j] = 0;
+                }
+                
+            
+            }
+
+        }
+        return enum_map;
+    }
+
+    
+}
+
+export enum Direction{
+    NORTH = 0,
+    NORTHEAST = 1,
+    EAST = 2,
+    SOUTHEAST = 3,
+    SOUTH = 4,
+    SOUTHWEST = 5,
+    WEST = 6,
+    NORTHWEST = 7
+}
+
+
+export enum Result {
+    PLAYER_A = 0,
+    PLAYER_B = 1,
+    TIE = 2,
+    ERROR = 3
+}
---
+++ src/replay/process_replay.ts
@@ -0,0 +1,89 @@
+import fs from 'fs'
+import {Direction, Board, Map} from './game_engine'
+
+
+class Match{
+    match_states: MatchState[];
+    bid_a: number;
+    bid_b: number;
+    win_reason: string;
+
+    constructor(match_states:MatchState[], bid_a: number, bid_b: number, win_reason:string){
+        this.match_states = match_states;
+        this.bid_a = bid_a;
+        this.bid_b = bid_b;
+        this.win_reason = win_reason;
+    }
+
+}
+
+class MatchState{
+    time_a: number;
+    time_b: number;
+    map_state: number[][];
+    dir_a: Direction;
+    dir_b: Direction;
+    round_num: number;
+    a_to_play: boolean;
+    a_apples_eaten: number;
+    b_apples_eaten: number;
+    a_length: number;
+    b_length: number;
+
+    constructor(b:Board, a_length:number, b_length:number){
+        this.time_a = b.a_time;
+        this.time_b = b.b_time;
+        this.dir_a = b.snake_a.dir;
+        this.dir_b = b.snake_b.dir;
+        this.round_num = b.get_round_num();
+        this.a_to_play = b.is_as_turn;
+
+        this.a_apples_eaten = b.snake_a.apples_eaten;
+        this.b_apples_eaten = b.snake_b.apples_eaten;
+
+        this.a_length = a_length;
+        this.b_length = b_length;
+
+        this.map_state = b.get_enum_map();
+        
+    }
+
+    
+}
+
+export function processData(historyFile: string): Match {
+    const fileContent = fs.readFileSync(historyFile, 'utf-8');
+    const history = JSON.parse(fileContent) as BoardHistory;
+
+    let match_states: MatchState[] = new Array(history.turn_count+1).fill(null);
+
+    let m:Map = new Map(history.game_map);
+    let b:Board = new Board(m, history.a_start, history.start_time)
+
+    if(history.turn_count>0){
+        match_states[0] = new MatchState(b, history.a_length[0], history.b_length[0])
+        for(let i:number = 1; i<= history.turn_count; i++){
+            b.play_turn(history.moves[i-1], history.cells_lost[i-1], history.times[i-1]);
+            match_states[i] = new MatchState(b, history.a_length[i], history.b_length[i]);
+        }
+    }
+
+    return new Match(match_states, history.bidA, history.bidB, history.reason);
+
+}
+
+interface BoardHistory{
+    moves:number[][],
+    times:number[],
+    game_map: string,
+    bidA: number,
+    bidB: number,
+    a_start:boolean,
+    result: number,
+    reason: string,
+    turn_count: number,
+    start_time: number,
+    cells_lost: number[][][],
+    a_length: number[],
+    b_length: number[],
+}
