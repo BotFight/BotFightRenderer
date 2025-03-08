53460cdb2c6253bf5e80175f1b1588a9d120be97
Tkwok7 <tkwok7@gatech.edu>
2025-03-08 16:33:43 -0500

    make stats look better

--- src/components/PlayerStats.js
+++ src/components/PlayerStats.js
@@ -54,14 +54,20 @@ export default function PlayerStats({ currentMatchStateIndex, matchStates }) {
   return (
     <div className="player-stats p-4 border rounded shadow-md w-full max-w-md text-white space-y-2">
       <h2 className="text-lg font-bold mb-4">Player Stats</h2>
-      <div className="flex flex-col gap-2">
-        <div>
-          <strong>Player  <span className='text-green-500'>A</span>:</strong> 
-          <span> Apples Eaten: {a_apples_eaten}, Length: {a_length}</span>
+      <div className="flex flex-col gap-2 pr-4">
+        <div className = "flex flex-row space-x-2">
+          <strong>Player  <span className='text-green-500'>A</span>:</strong>
+          <div className='flex flex-row space-x-2'>
+            <strong> Apples Eaten: {a_apples_eaten}</strong>
+            <strong>Length: {a_length}</strong>
+          </div> 
         </div>
-        <div>
+        <div className='flex flex-row space-x-2'>
           <strong>Player <span className='text-blue-500'>B</span>:</strong> 
-          <span> Apples Eaten: {b_apples_eaten}, Length: {b_length}</span>
+          <div className='flex flex-row space-x-2'>
+            <strong>Apples Eaten: {b_apples_eaten}</strong>
+            <strong>Length: {b_length}</strong>
+          </div>
         </div>
         <div>
           <strong>Turn:</strong> {a_to_play ?  <span className='text-green-500'>A</span> : <span className='text-blue-500'>B</span>}
