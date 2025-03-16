b32407ceb9141d9ad530c2af70ce9c97ef5d2aba
Tkwok7 <tkwok7@gatech.edu>
2025-03-15 22:39:24 -0400

    delete important stuff

--- package-mac-signed.sh
+++
@@ -1,6 +0,0 @@
-export APPLE_ID="miguel.schlicht@icloud.com"
-export APPLE_APP_SPECIFIC_PASSWORD="csfv-gvca-xwqp-binv"
-export APPLE_TEAM_ID="39X9RRX2UJ"
-export CSC_LINK="./Bytefight.p12"
-export CSC_KEY_PASSWORD="Bytefight69"
-npm run package
